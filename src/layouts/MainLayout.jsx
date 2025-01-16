027a7e7e8f97dfce22a6b2e79c0195785f8cba44
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-01-16 21:16:13 +0600

    banner done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../pages/shared/NavBar/NavBar";
+import Footer from "../pages/shared/Footer/Footer";
 
 
 
@@ -7,10 +8,10 @@ const MainLayout = () => {
     return (
         <>
             <NavBar></NavBar>
-            <div className="min-h-screen">
+            <div className="min-h-screen bg-emerald-100">
                 <Outlet></Outlet>
             </div>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import Banner from "../Banner/Banner";
+// import Banner from "../Banner/Banner";
 // import Banner from "../../components/Banner";
 // import Category from "../../components/Category";
 // import Featured from "./Featured/Featured";
@@ -13,9 +15,11 @@ const Home = () => {
             <Helmet>
                 <title>CureMedix | Home</title>
             </Helmet>
-            <h1 className="text-7xl text-center my-10">CureMedix HOME</h1>
-            {/* <Banner></Banner>
-            <Category></Category>
+            {/* <h1 className="text-7xl text-center my-10">CureMedix HOME</h1> */}
+            <div className="max-w-screen-2xl mx-auto">
+                <Banner></Banner>
+            </div>
+            {/* <Category></Category>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
             <Testimonials></Testimonials> */}
