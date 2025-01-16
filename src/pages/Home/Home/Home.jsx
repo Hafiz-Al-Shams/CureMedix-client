a6ad8b0785ff7da542a3ae43cbed04b4f2197359
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-01-16 21:48:53 +0600

    discount section done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
+import Discount from "../Discount/Discount";
 // import Banner from "../Banner/Banner";
 // import Banner from "../../components/Banner";
 // import Category from "../../components/Category";
@@ -18,9 +19,12 @@ const Home = () => {
             {/* <h1 className="text-7xl text-center my-10">CureMedix HOME</h1> */}
             <div className="max-w-screen-2xl mx-auto">
                 <Banner></Banner>
+                <div className="bg-base-100 pt-10">
+                    <h2 className="text-center text-3xl font-semibold pb-6">discounted products</h2>
+                    <Discount></Discount>
+                </div>
             </div>
-            {/* <Category></Category>
-            <PopularMenu></PopularMenu>
+            {/* <PopularMenu></PopularMenu>
             <Featured></Featured>
             <Testimonials></Testimonials> */}
         </div>
