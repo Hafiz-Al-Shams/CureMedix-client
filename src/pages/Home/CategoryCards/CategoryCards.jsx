83db1ef86c242aafca74830600ee0b9670d15476
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-01-27 01:52:49 +0600

    changed category cards design

--- src/pages/Home/CategoryCards/CategoryCards.jsx
+++ src/pages/Home/CategoryCards/CategoryCards.jsx
@@ -32,24 +32,27 @@ const CategoryCards = () => {
 
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-6">
-            {categories.map((category, i) => (
-                <div key={i} className="card bg-base-100 shadow-xl rounded-lg">
-                    <figure className="w-full">
-                        <img src={category.image} alt={category.name} className="w-full h-52 object-cover rounded-t-lg" />
-                    </figure>
-                    <div className="card-body p-4 space-y-2">
-                        <h2 className="card-title text-xl font-semibold">{category.name}</h2>
-                        <p className="text-sm text-gray-600">{category.count} medicines available</p>
-                        <div className="card-actions justify-end">
-                            <Link to={`/categoryDetails/${category.name}`}>
-                                <button className="btn btn-primary text-white">View Details</button>
-                            </Link>
+        <>
+            <h2 className="text-5xl font-semibold text-center mt-28 mb-10">Our bestsellers</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-6 pt-6 pb-28">
+                {categories.map((category, i) => (
+                    <div key={i} className="card bg-base-100 shadow-xl rounded-lg">
+                        <figure className="w-full">
+                            <img src={category.image} alt={category.name} className="w-full h-52 object-cover rounded-t-lg" />
+                        </figure>
+                        <div className="card-body p-4 space-y-2">
+                            <h2 className="card-title text-xl font-semibold">{category.name}</h2>
+                            <p className="text-sm text-gray-600">{category.count} medicines available</p>
+                            <div className="card-actions justify-end">
+                                <Link to={`/categoryDetails/${category.name}`}>
+                                    <button className="btn btn-primary text-white">View Details</button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </>
 
     );
 };
