550703d846878a6943590659879e543498d45e58
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-01-28 20:15:20 +0600

    again edited banner slider

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,11 +1,11 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import img1 from '../../../assets/home/01.png'
-import img2 from '../../../assets/home/02.png'
-import img3 from '../../../assets/home/03.png'
-import img4 from '../../../assets/home/04.png'
-import img5 from '../../../assets/home/05.png'
+// import img1 from '../../../assets/home/01.png'
+// import img2 from '../../../assets/home/02.png'
+// import img3 from '../../../assets/home/03.png'
+// import img4 from '../../../assets/home/04.png'
+// import img5 from '../../../assets/home/05.png'
 
 
 const Banner = () => {
@@ -18,22 +18,22 @@ const Banner = () => {
             transitionTime={500}
         >
             <div>
-                <img src={img1} alt="" />
+                <img src="https://i.ibb.co.com/2kHCxZ5/01.png" alt="" />
             </div>
             <div>
-                <img src={img2} />
+                <img src="https://i.ibb.co.com/16TbRKP/02.png" />
             </div>
             <div>
-                <img src={img3} />
+                <img src="https://i.ibb.co.com/20rbCZ2/03.png" />
             </div>
             <div>
-                <img src={img4} />
+                <img src="https://i.ibb.co.com/4MVyHhj/04.png" />
             </div>
             <div>
-                <img src={img5} />
+                <img src="https://i.ibb.co.com/tmx4vZ1/05.png" />
             </div>
             <div>
-                <img src={img3} />
+                <img src="https://i.ibb.co.com/20rbCZ2/03.png" />
             </div>
         </Carousel>
     );
